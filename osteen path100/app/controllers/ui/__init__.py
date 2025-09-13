1dfc1253a3077b42e2a2f383c16f21cd943cac7f
alex.su <118608061+codebdbd@users.noreply.github.com>
2025-09-13 03:35:57 +0300

    6799977878

--- osteen path100/app/controllers/ui/__init__.py
+++
@@ -1,4 +0,0 @@
-# app/controllers/ui/__init__.py
-
-from .menu_controller import MenuController as MenuController
-from .theme_controller import ThemeController as ThemeController
--- osteen path100/app/utils/logging/__init__.py
+++
@@ -1,4 +0,0 @@
-# app/utils/logging/__init__.py
-
-from .application_logger import ApplicationLogger as ApplicationLogger
-from .exception_handler import ExceptionHandler as ExceptionHandler
